78eaa2315bb9d54943681bcafead820637d02481
aleenam2 <100167576+aleenam2@users.noreply.github.com>
2023-04-28 15:57:45 -0500

    Add files via upload
    right before we add the second map

--- zelda_rom.sv
+++ zelda_rom.sv
@@ -493,3 +493,17 @@ end
 endmodule
 
 
+module background_2_rom (
+	input logic clock,
+	input logic [16:0] address,
+	output logic [4:0] q
+);
+
+logic [4:0] memory [0:89999] /* synthesis ram_init_file = "./background_2/background_2.mif" */;
+
+always_ff @ (posedge clock) begin
+	q <= memory[address];
+end
+
+endmodule
+
